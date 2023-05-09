cc65674f565ef473bc9898aca9f7740bb378d4e9
GabelGwlhs <113708877+GabelGwlhs@users.noreply.github.com>
2023-05-09 09:30:47 -0700

    Update temp.cpp

--- Unit1/temp.cpp
+++ Unit1/temp.cpp
@@ -2,16 +2,8 @@
 
 int main() {
   
-  double tempf;
-  double tempc;
+  double tempf = 63;
+  double tempc= (tempf-32)/1.8;//changes farenhit into celcuis
+  std::cout << "The temp is "<<tempc<<" degrees Celsius.";
   
-  // Ask the user
-  std::cout<<"Enter a temperature in new york:\n";
-  std::cin>>tempf;//user enters temp and assigns it to tempf
-  
-  
-  tempc = (tempf - 32) / 1.8;//changes it to celcius
-  
-  std::cout << "The temp is " << tempc << " degrees Celsius.\n";
-  //prints the temp in celcius
 }
