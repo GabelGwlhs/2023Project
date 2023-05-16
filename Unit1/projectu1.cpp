83bf41194724c740977330751b1ebf51116dcfcb
GabelGwlhs <113708877+GabelGwlhs@users.noreply.github.com>
2023-05-16 08:44:38 -0700

    Update projectu1.cpp

--- Unit1/projectu1.cpp
+++ Unit1/projectu1.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include<cmath>
+#include<math.h>
 
 int main() {
 
@@ -8,19 +8,19 @@ int main() {
     std::cout<<"Please enter a whole number: ";
     std::cin>>num;//assigns entered num to num
 
-    std::cout<<"A square with side length of"<<num;//computes values for a square and prints
-    std::cout<<"\n\t  has a perimeter of "<<(4*num);
+    std::cout<<"A square with side length of "<<num;//computes values for a square and prints
+    std::cout<<"\n\t has a perimeter of "<<(4*num);
     std::cout<<"\n\t has a area of "<<(num*num)<<"\n";
     std::cout<<"A circle with a diameter of "<<num;//computes values of a circle and prints
     double rad = num/2;
-    std::cout<<"\n\t  has a radius of "<<rad;
+    std::cout<<"\n\thas a radius of "<<rad;
     std::cout<<"\n\thas a circumference of "<<(2*pi*rad);
-    std::cout<<"\n\t has an area of "<<(pi*rad*rad);
+    std::cout<<"\n\thas an area of "<<(pi*rad*rad);
     //An equilateral triangle with side length of 7
     //    has a perimeter of 21
     //    has an area of 21.218
-    std::cout<<"\n An equilateral triangle with a side length of "<<num;
-    std::cout<<"\n\t has a perimeter of "<<num*3;
-    std::cout<<"\n\t has an area of "<<(.5)(pow(num,2));
+    std::cout<<"\nAn equilateral triangle with a side length of "<<num;//calculates values for equilateral triangle
+    std::cout<<"\n\thas a perimeter of "<<num*3;
+    std::cout<<"\n\thas an area of "<<(.5)*(pow(num,2));
 
 }
